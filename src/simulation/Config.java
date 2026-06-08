e2d5bd0815c49ba66c30141a0403b8240d5195e0
Amin <m.amin.mrezaie@gmail.com>
2026-06-08 15:50:00 +0330

    add simulation configuration for elevator system parameters

---
+++ src/simulation/Config.java
@@ -0,0 +1,16 @@
+package simulation;
+
+public class Config {
+    public int    numFloors            = 8;
+    public int    numGeneralElevators  = 2;
+    public int    numFacultyElevators  = 1;
+    public int    numFreightElevators  = 1;
+    public int    numStudents          = 10;
+    public int    numProfessors        = 4;
+    public int    numStaff             = 3;
+    public int    numPorters           = 2;
+    public int    numRepairmen         = 2;
+    public long   elevatorSpeedMs      = 400;
+    public double maxWeightGeneral     = 500;
+    public double maxWeightFaculty     = 400;
+    public double maxWeightFreight     = 1000;}
